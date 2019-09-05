c99f35c83f80bab2d19920f256a1f478312ff195
johan gace <johangace@johans-MacBook-Pro.local>
2019-09-05 13:29:56 -0400

    user can see shouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   include Clearance::User
 
   validates :username, presence: true, uniqueness: true
+  has_many :shouts, dependent: :destroy
+
 
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#show"
+  resources :shouts , only: [:create]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
-
+ 
   resources :users, only: [:create] do
     resource :password,
       controller: "clearance/passwords",
